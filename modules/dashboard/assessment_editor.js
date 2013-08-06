0934ee131176549e8643a9842de4376915b25e6a
sara rubin <sararubin82@gmail.com>
2013-08-06 02:01:39 -0400

    added under coursebuilder directory

--- modules/dashboard/assessment_editor.js
+++
@@ -1,9 +0,0 @@
-var controller = new AssessmentEditorController(
-    Y,
-    cb_global.form,
-    cb_global.xsrf_token,
-    function(s) { return window.confirm(s); },
-    cbShowMsg,
-    formatServerErrorMessage
-);
-controller.init();
